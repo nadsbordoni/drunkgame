7f4e96392dd62ac81f14e418f8a1c6a600a02798
nadsbordoni <nfb@cin.ufpe.br>
2020-08-28 15:50:20 -0300

    Telas e fontes

---
+++ DrinkGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DrinkGame
+//
+//  Created by Nádia Bordoni on 28/08/20.
+//  Copyright © 2020 Nádia Bordoni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
