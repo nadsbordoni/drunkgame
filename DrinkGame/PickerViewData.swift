4795447af2a6d82aea5474898d8a809f78c48013
nadsbordoni <nfb@cin.ufpe.br>
2020-08-31 19:52:20 -0300

    arquivos design, navegação, struct arrays

---
+++ DrinkGame/PickerViewData.swift
@@ -0,0 +1,16 @@
+//
+//  PickerViewData.swift
+//  DrinkGame
+//
+//  Created by Nádia Bordoni on 31/08/20.
+//  Copyright © 2020 Nádia Bordoni. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Pickerdata {
+    static let action = ["Come farofa", "Faz ode à batata", "Pula como Saci", "Alisa a barriga", "Faz o quatro", "Planta bananeira", "Imita personalidade famosa", "Faz quadradinho de oito", ""]
+    
+    static let music = [""]
+}
